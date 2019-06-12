35138e3099b80b012a98a1f857fcff39d0687dd5
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-12 04:50:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,10 @@ id_generator = {
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+my_hash_creator = {
+  :name => "Grace Hopper"
+  :paintings => 10
+}
 end
 
 def read_from_hash(hash, key)
